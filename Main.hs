83f2ebbef54ab525c229781acf2a50764e1d969a
Fylwind <fyl@wolfpa.ws>
2015-03-20 22:06:07 -0400

    Refactor Twitter stuff into its own module

--- Main.hs
+++ Main.hs
@@ -1,121 +1,66 @@
-{-# LANGUAGE FlexibleContexts
-           , NoMonomorphismRestriction
-           , OverloadedStrings
-           , ScopedTypeVariables #-}
+{-# LANGUAGE
+    ConstraintKinds
+  , FlexibleContexts
+  , NoMonomorphismRestriction
+  , OverloadedStrings #-}
 module Main (main) where
 import Control.Applicative ((<*), (<*>), (*>), (<|>), some, many)
 import Control.Lens
-import Control.Monad (liftM)
+import Control.Monad.Catch (catch, catchIOError)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Reader (ReaderT(runReaderT))
-import Control.Monad.Reader.Class
-import Control.Monad.Trans.Resource (MonadResource)
-import Data.Conduit
-import Data.Default (def)
 import Data.Foldable (traverse_)
-import Data.Functor ((<$>), void)
+import Data.Functor ((<$>))
 import Data.Monoid ((<>), mempty)
 import Data.Text (Text)
-import Network.HTTP.Conduit
 import System.Directory
 import System.FilePath
 import System.IO (hFlush, hPutStrLn, stderr, stdout)
-import Control.Monad.Catch (MonadCatch, catch, catchIOError)
-import Web.Authenticate.OAuth (OAuth(..), Credential(..))
-import Web.Twitter.Conduit
-import Web.Twitter.Types.Lens hiding (name)
+import Web.Twitter.Types.Lens
 import qualified Data.ByteString.Char8 as ByteString
-import qualified Data.Conduit.List as C
-import qualified Data.List as List
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import qualified Text.Parsec as P
-import qualified Web.Authenticate.OAuth as OAuth
-import qualified Keys
 
-class HasManager a where
-  manager :: IndexPreservingGetter a Manager
+import Twitter
 
-oauth :: OAuth
-oauth = twitterOAuth { oauthConsumerKey    = ByteString.pack Keys.consumerKey
-                     , oauthConsumerSecret = ByteString.pack Keys.consumerSecret }
+ensureDirectoryExist :: MonadIO io => FilePath -> io FilePath
+ensureDirectoryExist dir = do
+  liftIO (createDirectoryIfMissing True dir)
+  return dir
 
-authorize :: (MonadIO io, MonadReader env io, HasManager env) =>
-             (String -> io String)      -- ^ PIN prompt: @URL -> m PIN@
-          -> io Credential
-authorize getPIN = do
-  mgr  <- (^. manager) <$> ask
-  cred <- OAuth.getTemporaryCredential oauth mgr
-  pin  <- getPIN (makeAuthUrl cred)
-  OAuth.getAccessToken oauth (combinePinCred pin cred) mgr
-  where
-
-    (<$>) = liftM                       -- for backward compatibility only
-
-    makeAuthUrl cred =
-      takeWhile (/= '&') (OAuth.authorizeUrl oauth cred)
-
-    combinePinCred pin cred =
-      OAuth.insert "oauth_verifier" (ByteString.pack pin) cred
-
-twitterLogin :: (MonadIO io, MonadReader env io, HasManager env) =>
-                FilePath
-             -> io TWInfo
-twitterLogin keysFile = do
+twitterAuth :: MonadManager r m => FilePath -> m TWInfo
+twitterAuth keysFile = do
 
   -- read the keys from file
-  keys <- liftIO $ ByteString.readFile keysFile
-                   `catchIOError` \ _ -> return mempty
+  keys <- liftIO $
+    ByteString.readFile keysFile
+    `catchIOError` \ _ -> return mempty
 
+  -- check whether the keys are stored or absent
   cred <- case ByteString.lines keys of
-    -- if keys are stored
-    token : secret : _ -> return (makeAuth token secret)
-    -- if keys are absent
-    _                  -> requestAuth
-
-  liftIO $ do
-    putStr "Login successful.\n\n"
-    hFlush stdout
-  return (setCredential oauth cred def)
-
-  where
-
-    requestAuth = do
-      cred <- authorize $ \ url -> liftIO $ do
+    token : secret : _ -> return (token, secret)
+    _                  -> do
+      (cred, url) <- preauthorize
+      pin <- liftIO $ do
         putStr ("1. Go to this URL (all on one line):\n\n" <>
                 url <> "\n\n" <>
                 "2. Enter the PIN here:\n\nPIN> ")
         hFlush stdout
         getLine
-      accessToken  <- lookupCred cred "oauth_token"
-      accessSecret <- lookupCred cred "oauth_token_secret"
-      let keys' = ByteString.unlines [accessToken, accessSecret]
+      (token, secret) <- authorize cred pin
+      let keys' = ByteString.unlines [token, secret]
       liftIO $ do
         ByteString.writeFile keysFile keys'
         putStr ("\nAccess keys saved to " <> keysFile <> ".\n\n")
-      return cred
+      return (token, secret)
 
-    makeAuth token secret =
-      Credential [("oauth_token", token), ("oauth_token_secret", secret)]
+  liftIO $ do
+    putStr "Login successful.\n\n"
+    hFlush stdout
+  return (newTWInfo cred)
 
-    lookupCred (Credential cred) name =
-      case List.lookup name cred of
-        Just value -> return value
-        Nothing    -> liftIO . ioError . userError $
-                      "failed to receive access keys"
-
-ensureDirectoryExist :: MonadIO io => FilePath -> io FilePath
-ensureDirectoryExist dir = do
-  liftIO $ createDirectoryIfMissing True dir
-  return dir
-
-processTimeline :: (MonadIO io, MonadCatch io, MonadResource io,
-                    MonadReader env io, HasManager env) =>
-                   Text
-                -> TWInfo
-                -> StreamingAPI
-                -> io ()
-processTimeline thisScreenName twitter event = case event of
+processTimeline :: MonadTwitter r m => Text -> StreamingAPI -> m ()
+processTimeline thisScreenName event = case event of
   SStatus status ->
     let pStatusText = (,,) <$> pScreenName <*> many pScreenName <*> pMessage
         pScreenName = Text.pack <$>
@@ -133,18 +78,16 @@ processTimeline thisScreenName twitter event = case event of
 
           | screenName == thisScreenName && message == ":3" ->
 
-            let tweet msg = do
-                  mgr <- (^. manager) <$> ask
+            let tweet msg =
                   let fullMsg = Text.unwords $
                         (("@" <>) <$> sName : otherNames) <> [msg]
-                    in void . call twitter mgr $
-                       update fullMsg
-                       & inReplyToStatusId ?~ sId
+                  in postReply fullMsg sId
 
+                retry :: MonadTwitter r m => Int -> Text -> m ()
                 retry n msg =
                   tweet msg `catch` \ err ->
-                  if twitterErrorCodeIs errStatusDuplicate err && n > 0
-                  then retry (n - 1 :: Int) (" " <> msg)
+                  if errorCodeIs errStatusDuplicate err && n > 0
+                  then retry (n - 1) (" " <> msg)
                   else logTwitterError err
 
             in do
@@ -156,35 +99,17 @@ processTimeline thisScreenName twitter event = case event of
         _ -> return ()
   _ -> return ()
 
-errStatusDuplicate :: Int
-errStatusDuplicate = 187
-
-twitterErrorCodeIs :: Int -> TwitterError -> Bool
-twitterErrorCodeIs code err = case err of
-  TwitterErrorResponse _ _
-    [TwitterErrorMessage code' _] -> code == code'
-  _                               -> False
-
-logTwitterError :: MonadIO io => TwitterError -> io ()
+logTwitterError :: MonadIO m => TwitterError -> m ()
 logTwitterError err = liftIO $ do
   case err of
-    TwitterErrorResponse _ _ msgs ->
-      (Text.hPutStrLn stderr . twitterErrorMessage) `traverse_` msgs
+    TwitterErrorResponse _ _ msgs
+      -> traverse_ (Text.hPutStrLn stderr . twitterErrorMessage) msgs
     _ -> hPutStrLn stderr (show err)
   hFlush stderr
 
-data Env = Env { _manager :: Manager }
-
-instance HasManager Env where
-  manager = to _manager
-
 main :: IO ()
 main = do
-  confDir <- ensureDirectoryExist =<< liftIO (getAppUserDataDirectory "c3r")
-  withManager $ \ mgr -> (`runReaderT` Env mgr) $ do
-    let keysFile = confDir </> "keys"
-    twitter <- twitterLogin keysFile
-    thisUser <- call twitter mgr accountVerifyCredentials
-    let thisScreenName = thisUser ^. userScreenName
-    streamSource <- stream twitter mgr userstream
-    streamSource $$+- C.mapM_ (processTimeline thisScreenName twitter)
+  confDir <- ensureDirectoryExist =<< getAppUserDataDirectory "config/c3r"
+  runTwitterM (twitterAuth (confDir </> "keys")) $ do
+    myName <- getMyName
+    userStream (processTimeline myName)
---
+++ Twitter.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE
+    ConstraintKinds
+  , FlexibleContexts
+  , FlexibleInstances
+  , NoMonomorphismRestriction
+  , OverloadedStrings #-}
+module Twitter
+  ( -- * Main module
+    module Twitter
+    -- * Re-exports
+  , TWInfo
+  , TwitterError(..)
+  , twitterErrorMessage
+  ) where
+import Control.Lens
+import Control.Monad.Catch (MonadCatch)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (ReaderT, runReaderT)
+import Control.Monad.Reader.Class
+import Control.Monad.Trans.Resource (MonadResource, ResourceT)
+import Data.ByteString (ByteString)
+import Data.Conduit (($$+-))
+import Data.Default (def)
+import Data.Functor (void)
+import Data.Text (Text)
+import Network.HTTP.Conduit
+import Web.Authenticate.OAuth (OAuth(..), Credential(..))
+import Web.Twitter.Conduit
+import Web.Twitter.Types.Lens hiding (name)
+import qualified Data.ByteString.Char8 as ByteString
+import qualified Data.Conduit.List as C
+import qualified Data.List as List
+import qualified Web.Authenticate.OAuth as OAuth
+import qualified Keys
+
+-- | 'Monad' within which HTTP operations may be performed.
+type ManagerM a = ReaderT Manager (ResourceT IO) a
+
+-- | 'Monad' within which Twitter operations may be performed.
+type TwitterM a = ReaderT (TWInfo, Manager) (ResourceT IO) a
+
+-- | 'Monad's that have a 'Manager' for HTTP connections.
+--
+--   Note: this is implicitly a subclass of 'MonadIO'.
+type MonadManager r m = (HasManager r, MonadReader r m,
+                         MonadCatch m, MonadResource m)
+
+-- | 'Monad's that have a 'TWInfo' for HTTP connections.
+--
+--   Note: this is implicitly a subclass of 'MonadIO'.
+type MonadTwitter r m = (HasTwitter r, MonadManager r m)
+
+class HasManager a where
+  manager :: Getter a Manager
+
+instance HasManager Manager where
+  manager = id
+
+instance HasManager (a, Manager) where
+  manager = _2
+
+class HasTwitter a where
+  twitter :: Getter a TWInfo
+
+instance HasTwitter TWInfo where
+  twitter = id
+
+instance HasTwitter (TWInfo, a) where
+  twitter = _1
+
+-- | Run the 'TwitterM' monad.
+runTwitterM :: MonadIO m => ManagerM TWInfo -> TwitterM () -> m ()
+runTwitterM getTwit action = liftIO . withManager $ \ mgr -> do
+  tw <- runReaderT getTwit mgr
+  runReaderT action (tw, mgr)
+
+oauth :: OAuth
+oauth = twitterOAuth
+  { oauthConsumerKey    = ByteString.pack Keys.consumerKey
+  , oauthConsumerSecret = ByteString.pack Keys.consumerSecret }
+
+preauthorize :: MonadManager r m => m (Credential, String)
+preauthorize = do
+  mgr  <- view manager
+  cred <- OAuth.getTemporaryCredential oauth mgr
+  return (cred, takeWhile (/= '&') (OAuth.authorizeUrl oauth cred))
+
+authorize :: MonadManager r m =>
+             Credential -> String -> m (ByteString, ByteString)
+authorize cred pin = do
+  mgr    <- view manager
+  cred   <- OAuth.getAccessToken oauth (combinePinCred pin cred) mgr
+  token  <- lookupCred cred "oauth_token"
+  secret <- lookupCred cred "oauth_token_secret"
+  return (token, secret)
+  where combinePinCred pin cred =
+          OAuth.insert "oauth_verifier" (ByteString.pack pin) cred
+        lookupCred (Credential cred) name =
+          case List.lookup name cred of
+            Just value -> return value
+            Nothing    -> liftIO . ioError . userError $
+                          "failed to receive access keys"
+
+newTWInfo :: (ByteString, ByteString) -> TWInfo
+newTWInfo (token, secret) = setCredential oauth (Credential cred) def
+  where cred = [("oauth_token", token), ("oauth_token_secret", secret)]
+
+-- | Convenience function for extracting the environment.
+withTwitter :: MonadTwitter r m => (TWInfo -> Manager -> m a) -> m a
+withTwitter action = do
+  tw  <- view twitter
+  mgr <- view manager
+  action tw mgr
+
+getMyName :: MonadTwitter r m => m Text
+getMyName = withTwitter $ \ tw mgr -> do
+  me <- call tw mgr accountVerifyCredentials
+  return (me ^. userScreenName)
+
+userStream :: MonadTwitter r m => (StreamingAPI -> m ()) -> m ()
+userStream action = withTwitter $ \ tw mgr -> do
+  streamSource <- stream tw mgr userstream
+  streamSource $$+- C.mapM_ action
+
+postReply :: MonadTwitter r m => Text -> Integer -> m ()
+postReply msg id = withTwitter $ \ tw mgr -> do
+  void . call tw mgr $ update msg & inReplyToStatusId ?~ id
+
+errStatusDuplicate :: Int
+errStatusDuplicate = 187
+
+errorCodeIs :: Int -> TwitterError -> Bool
+errorCodeIs code err = case err of
+  TwitterErrorResponse _ _ [TwitterErrorMessage code' _]
+    -> code == code'
+  _ -> False
