78e2b66fbbd2698ae3b56791cc62f30a7e63f5af
Fylwind <fyl@wolfpa.ws>
2016-06-07 05:36:41 -0400

    Add c3r.service

--- deploy-insomnia.sh
+++ deploy-insomnia.sh
@@ -17,6 +17,7 @@ branch=${1-master}
 rsync -aPvc Keys.hs "$srv:$app/Keys.hs"
 ssh -T "$srv" <<EOF
 set -eu
+changed=
 
 sudo apt-get -y install libsqlite3-dev stack
 
@@ -31,37 +32,33 @@ stack upgrade
 stack --resolver=lts-6.1 init --force
 stack build
 
-sudo install -m755 "\`stack exec which $app\`" /usr/local/bin/$app
-sudo tee /etc/systemd/system/$app.service >/dev/null <<SERVICEEOF
-[Unit]
-After=network.target
+cmp "\`stack exec which $app\`" /usr/local/bin/$app >/dev/null 2>&1 || {
+    sudo install -m755 "\`stack exec which $app\`" /usr/local/bin/$app
+    changed=t
+}
 
-[Service]
-Type=simple
-Restart=always
-RestartSec=30
-ExecStart=/usr/local/bin/$app
-User=$app
-WorkingDirectory=/var/lib/$app
-
-[Install]
-WantedBy=default.target
-SERVICEEOF
+cmp $app.service /etc/systemd/system/$app.service >/dev/null 2>&1 || {
+    sudo cp $app.service /etc/systemd/system/$app.service
+    changed=t
+}
 
 groups $app >/dev/null 2>&1 ||
 sudo useradd -r -s /usr/sbin/nologin -m -d /var/lib/$app $app
 sudo chmod 700 /var/lib/$app
 sudo cp -r tools /var/lib/$app
 
-sudo systemctl daemon-reload
-sudo systemctl enable $app
-if sudo [ -f /var/lib/$app/$app.db ]; then
-    sudo systemctl restart $app
-else
-    printf '%s' 'Note: Service is not yet configured; run:
+if [ "\$changed" ]; then
+    sudo systemctl daemon-reload
+    sudo systemctl enable $app
+    if sudo [ -f /var/lib/$app/$app.db ]; then
+        sudo systemctl restart $app
+    else
+        printf '%s' 'Note: Service is not yet configured; to configure, run:
 
     (cd /var/lib/$app && sudo -u $app /usr/local/bin/$app)
+    systemctl start $app
 
-to configure.  Then, start the service using \`systemctl start $app\`.'
+'
+    fi
 fi
 EOF
